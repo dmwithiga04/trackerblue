020cfc32068dde60ea1e633c9aee2d67676429ca
dmwithiga04 <61370243+dmwithiga04@users.noreply.github.com>
2020-08-06 14:45:37 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 2) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    }
+})
 radio.setGroup(1)
+radio.setTransmitPower(1)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         radio.sendNumber(1)
